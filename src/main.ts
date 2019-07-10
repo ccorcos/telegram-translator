73559a301de81a0452d56cd9196e5f0aee9e965e
Chet Corcos <ccorcos@gmail.com>
2019-07-09 22:38:09 -0700

    Credentials

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { Request, Response } from "express"
 import { Translate } from "@google-cloud/translate"
 
-const projectId = process.env.GCLOUD_PROJECT
-
 /**
  * The name of this export must match the `--entry-point` option in
  * the deploy script.
  */
 export default async function main(req: Request, res: Response) {
-	const translate = new Translate({ projectId })
-	const text = "Hello, world!"
-	const target = "ru"
+	const translate = new Translate()
+	const text = "Привет, мир!"
+	const target = "en"
 
-	console.log(process.env)
 	console.log(req)
 
 	// Translates some text into Russian
